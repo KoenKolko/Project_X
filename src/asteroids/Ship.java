c215b47632d063bbc58ea307844ad98fe9c89b8c
Koen Jacobs <koen.jacobs.kolko@gmail.com>
2013-03-03 23:13:37 +0100

    Done for the night...

--- src/asteroids/Ship.java
+++ src/asteroids/Ship.java
@@ -9,5 +9,77 @@ package asteroids;
  */
 
 public class Ship implements IShip {
+	
+	private double x;
+	private double y;
+	private double xVelocity;
+	private double yVelocity;
+	private double radius;
+	private double angle;
+	
+	
+	public Ship(double x, double y, double xVelocity, double yVelocity, double radius, double angle)
+	{		
+		this.setX(x);
+		this.setY(y);
+		this.setXVelocity(xVelocity);
+		this.setYVelocity(yVelocity);
+		this.setRadius(radius);
+		this.setAngle(angle);		
+	}
+	
+	private boolean isValidRadius(double radius)
+	{
+		return (radius >= 10);
+	}
 
+	public double getX() {
+		return x;
+	}
+
+	public void setX(double x) {
+		this.x = x;
+	}
+
+	public double getY() {
+		return y;
+	}
+
+	public void setY(double y) {
+		this.y = y;
+	}
+
+	public double getXVelocity() {
+		return xVelocity;
+	}
+
+	public void setXVelocity(double xVelocity) {
+		this.xVelocity = xVelocity;
+	}
+
+	public double getYVelocity() {
+		return yVelocity;
+	}
+
+	public void setYVelocity(double yVelocity) {
+		this.yVelocity = yVelocity;
+	}
+
+	public double getAngle() {
+		return angle;
+	}
+
+	public void setAngle(double angle) {
+		this.angle = angle;
+	}
+
+	public double getRadius() {
+		return radius;
+	}
+
+	public void setRadius(double radius) {
+		this.radius = radius;
+	}
+	
 }
+ 
