1e73ace1d33e13f086f51c38bb54bf6c5ffeae18
Yannick <yannick-hd@hotmail.com>
2013-03-17 14:56:40 +0100

    Adjustments in class DoubleCalculator

--- src/asteroids/DoubleCalculator.java
+++ src/asteroids/DoubleCalculator.java
@@ -1,60 +1,42 @@
 package asteroids;
 
 public class DoubleCalculator {
-	
+
 	public Double addDoubles(double x, double y) throws ArithmeticException{
 		double sum = x + y;
 		if(x>0 && y>0){
-			if(sum > 0){
+			if(sum > 0)
 				return sum;
-			}
-			else{
-				throw new ArithmeticException();
-			}
+			else throw new ArithmeticException();
 		}
 		else if(x<0 && y<0){
-			if(sum < 0){
+			if(sum < 0)
 				return sum;
-			}
-			else{
-				throw new ArithmeticException();
-			}
-			
+			else throw new ArithmeticException();
 		}
-		else if(x==0){
-			return y;
-		}
-		else if(y==0){
-			return x;
-		}		
-		else{
-			return sum;
-		}
+		else if(x==0) return y;
+		else if(y==0) return x;
+		else return sum;
+
 	}
-	
+
 	public Double multiplyDoubles(double x, double y) throws ArithmeticException{
 		double product = x * y;
 		if((x>0 && y>0) || (x<0 && y<0)){
-			if(product > 0){
+			if(product > 0) 
 				return product;
-			}
-			else{
+			else 
 				throw new ArithmeticException();
-			}				
 		}
 		else if((x>0 && y<0) || (x<0 && y>0)){
-			if(product < 0){
+			if(product < 0) 
 				return product;
-			}
-			else{
+			else 
 				throw new ArithmeticException();
-			}
 		}
-		else if(x==0 || y == 0){
+		else if(x==0 || y == 0) 
 			return 0.0;
-		}
-		else{
+		else 
 			return product;
-		}
 	}
 }
