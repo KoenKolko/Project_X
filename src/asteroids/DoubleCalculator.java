43a8db2a3c5ecaa42abe002ebb17c141038446e4
floppiedisc <yannick-hd@hotmail.com>
2013-03-17 11:08:12 -0700

    Test DoubleCalculator

--- src/asteroids/DoubleCalculator.java
+++ src/asteroids/DoubleCalculator.java
@@ -3,40 +3,15 @@ package asteroids;
 public class DoubleCalculator {
 
 	public Double addDoubles(double x, double y) throws ArithmeticException{
-		double sum = x + y;
-		if(x>0 && y>0){
-			if(sum > 0)
-				return sum;
-			else throw new ArithmeticException();
-		}
-		else if(x<0 && y<0){
-			if(sum < 0)
-				return sum;
-			else throw new ArithmeticException();
-		}
-		else if(x==0) return y;
-		else if(y==0) return x;
-		else return sum;
+		if ( (x == Double.POSITIVE_INFINITY && y == Double.NEGATIVE_INFINITY) || (y == Double.POSITIVE_INFINITY && x == Double.NEGATIVE_INFINITY))
+			throw new ArithmeticException();
+		return x + y;
 
 	}
 
 	public Double multiplyDoubles(double x, double y) throws ArithmeticException{
-		double product = x * y;
-		if((x>0 && y>0) || (x<0 && y<0)){
-			if(product > 0) 
-				return product;
-			else 
-				throw new ArithmeticException();
-		}
-		else if((x>0 && y<0) || (x<0 && y>0)){
-			if(product < 0) 
-				return product;
-			else 
-				throw new ArithmeticException();
-		}
-		else if(x==0 || y == 0) 
-			return 0.0;
-		else 
-			return product;
+		if ( (x == Double.POSITIVE_INFINITY && y == Double.NEGATIVE_INFINITY) || (y == Double.POSITIVE_INFINITY && x == Double.NEGATIVE_INFINITY))
+			throw new ArithmeticException();
+		return x * y;
 	}
 }
--- src/asteroids/test/DoubleCalculatorTest.java
+++ src/asteroids/test/DoubleCalculatorTest.java
@@ -6,18 +6,34 @@ import org.junit.Before;
 import org.junit.Test;
 
 import asteroids.DoubleCalculator;
+import asteroids.ModelException;
+import asteroids.Util;
 
 public class DoubleCalculatorTest {
 	DoubleCalculator calc;
-@Before
-public void setUp() throws Exception {
-	calc = new DoubleCalculator();
-}
+	@Before
+	public void setUp() throws Exception {
+		calc = new DoubleCalculator();
+	}
 
-@Test
-public void testAddFail() {
-	double maxDouble = Double.MAX_VALUE;	
-	calc.addDoubles(maxDouble,5);
+	@Test 
+	public void testAdd() {
+		assertEquals(8.0, calc.addDoubles(-6, 14), Util.EPSILON);
+	}
+	
+	@Test (expected=ArithmeticException.class)
+	public void testAddFail() {
+		calc.addDoubles(Double.NEGATIVE_INFINITY, Double.POSITIVE_INFINITY);
+	}
+	
+	@Test 
+	public void testMultiply() {
+		assertEquals(-12.0, calc.multiplyDoubles(-6, 2), Util.EPSILON);
+	}
+	
+	@Test (expected=ArithmeticException.class)
+	public void testMultiplyFail() {
+		calc.multiplyDoubles(Double.NEGATIVE_INFINITY, Double.POSITIVE_INFINITY);
 	}
 
 }
