12522e4a321f3f2e3f7ae2a627b19080e986af48
floppiedisc <yannick-hd@hotmail.com>
2013-05-17 16:16:28 +0200

    Update

--- src/asteroids/model/programs/expression/BinaryExpression.java
+++ src/asteroids/model/programs/expression/BinaryExpression.java
@@ -1,5 +1,32 @@
 package asteroids.model.programs.expression;
 
-public class BinaryExpression {
+public abstract class BinaryExpression extends ComposedExpression{
+
+	private Expression e1, e2;
+	
+	public BinaryExpression(int line, int column, Expression e1, Expression e2) {
+		super(line, column, e1, e2);
+		setE1(e1);
+		setE2(e2);
+		
+	}
+
+	public Expression getE1() {
+		return e1;
+	}
+
+	public void setE1(Expression e1) {
+		this.e1 = e1;
+	}
+
+	public Expression getE2() {
+		return e2;
+	}
+
+	public void setE2(Expression e2) {
+		this.e2 = e2;
+	}
+	
+	
 
 }
--- src/asteroids/model/programs/expression/UnaryExpression.java
+++ src/asteroids/model/programs/expression/UnaryExpression.java
@@ -1,5 +1,20 @@
 package asteroids.model.programs.expression;
 
-public class UnaryExpression {
+public abstract class UnaryExpression extends ComposedExpression {
+
+	private Expression e;
+	
+	public UnaryExpression(int line, int column, Expression e1, Expression e2) {
+		super(line, column, e1, e2);
+		setE(e);
+	}
+
+	public Expression getE() {
+		return e;
+	}
+
+	public void setE(Expression e) {
+		this.e = e;
+	}
 
 }
